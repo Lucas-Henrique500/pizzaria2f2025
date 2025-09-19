ad7fbd2fcea93c50185bbf6ad1b3ddb324c09009
Lucas-Henrique500 <rm95200@estudante.fieb.edu.br>
2025-09-19 07:47:53 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Produtos from "./pages/produtos"
+
 const App = () => {
 
   return (
     <>
-    <h1>Projeto Pizzaria</h1></>
+      <h1>Projeto Pizzaria</h1>
+      <Produtos />
+    </>
   )
 }
 
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,16 @@
+const Produtos = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>Pizza de mussarela</li>
+                <li>Pizza de calabresa</li>
+                <li>Pizza portugusa</li>
+                <li>Pizza baiana</li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Produtos
