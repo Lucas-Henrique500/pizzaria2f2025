bef766c9dc4993529d4e253ec10014184cd9ce7b
Lucas-Henrique500 <rm95200@estudante.fieb.edu.br>
2025-09-25 07:47:45 -0300

    catch

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,19 +1,33 @@
+//ferramenta de consumo de rotas (endpoints) do backend
 import axios from "axios"
+//hooks do react - estado e renderização
+import { useState, useEffect } from "react"
 
 const Produtos = () => {
+
+    //estado com lista de pizzas 
+    const [pizzas, setPizzas] = useState(['calabraza', 'muçarela', 'baiana'])
+
     //consumo de um endpoint com as pizzas
-    axios.get("http://viacep.com.br/ws/01010000/json")
+    //GET - busca os dados do endppint ou rota
+    //THEN - é uma promise - método assíncrono
+    //catch - tratamento de erros
+    useEffect(()=>{
+    axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
     .then(response=>{
-        console.log(response.data)
+        console.log(response.data.data)
+        setPizzas(responde.data.data)
     })
+    .catch(errors=>console.log(errors))
+    },[])
 
     //interação da lista de pizzas
-    const ListaPizzas = pizzas.map(pizza=><li>{pizza}</li>)
+    const listaPizzas = pizzas.map(pizza=><li>{pizza.nome}</li>)
 
     return (
         <div>
             <ul>
-                {ListaPizzas}
+                {listaPizzas}
             </ul>
         </div>
     )
